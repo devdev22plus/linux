da5d1bf17134a6089a4c130f476b700fa5bd7537
devdev22plus <x-cyber@windowslive.com>
2020-06-21 11:55:30 +0700

    update iptables

---
+++ iptables_setup/install.sh
@@ -0,0 +1,34 @@
+# upgrade nowmal
+yum upgrade -y
+yum update -y
+
+
+
+# install iptables
+yum install iptables-services
+systemctl enable iptables
+systemctl start iptables
+
+
+
+# stop firewalld
+systemctl disable firewalld
+systemctl stop firewalld
+
+
+#echo 1 > /proc/sys/net/ipv4/ip_forward
+sysctl -w net.ipv4.ip_forward=1
+
+
+# change ssh port here
+# vi /etc/ssh/sshd_config
+
+# help for this command https://stackoverflow.com/questions/525592/find-and-replace-inside-a-text-file-from-a-bash-command
+# auto find and replace
+sed -i -e 's/#Port 22/Port 42639/g' /etc/ssh/sshd_config
+
+
+iptables-restore < iptables
+service iptables save
+
+
